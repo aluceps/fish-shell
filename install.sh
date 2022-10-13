1174ec8d7cb674185bdc32076f3099e02eb58b67
atsushi.takano <atsushi.takano@every.tv>
2022-10-13 17:01:48 +0900

    update settings

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-cp -p function/ghq_select_repository.fish ~/.config/fish/functions/
-cp -p function/ghq_select_branch.fish ~/.config/fish/functions/
+# function
 cp -p function/convert_to_gif.fish ~/.config/fish/functions/
+cp -p function/ghq_select_branch.fish ~/.config/fish/functions/
+cp -p function/peco_select_history.fish ~/.config/fish/functions/
 
-cp -p function/ghq_select_branch_key_bindings.fish ~/.config/fish/conf.d/
-cp -p function/ghq_select_history_key_bindings.fish ~/.config/fish/conf.d/
-cp -p function/ghq_select_repository_key_bindings.fish ~/.config/fish/conf.d/
+# conf.d
+cp -p conf.d/ghq_key_bindings.fish ~/.config/fish/cunf.d/
+cp -p conf.d/ghq_select_branch_key_bindings.fish ~/.config/fish/cunf.d/
+cp -p conf.d/peco_select_history_key_bindings.fish ~/.config/fish/cunf.d/
 
-echo 'alias gl="ghq_select_repository"' >> ~/.config/fish/config.fish
-echo 'alias gc="ghq_select_branch"' >> ~/.config/fish/config.fish
+# alias
 echo 'alias convert_to_gif="convert_to_gif"' >> ~/.config/fish/config.fish
