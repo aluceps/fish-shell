8b4c984d058840242766439365ebe3a298e556f7
aluceps <aluceps@gmail.com>
2024-02-02 18:07:54 +0900

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 # function
 cp -p function/convert_to_gif.fish ~/.config/fish/functions/
-cp -p function/ghq_select_branch.fish ~/.config/fish/functions/
 cp -p function/peco_select_history.fish ~/.config/fish/functions/
+cp -p function/peco_select_branch.fish ~/.config/fish/functions/
+cp -p function/peco_select_repository.fish ~/.config/fish/functions/
 
 # conf.d
-cp -p conf.d/ghq_key_bindings.fish ~/.config/fish/conf.d/
-cp -p conf.d/ghq_select_branch_key_bindings.fish ~/.config/fish/conf.d/
 cp -p conf.d/peco_select_history_key_bindings.fish ~/.config/fish/conf.d/
+cp -p conf.d/peco_select_branch_key_bindings.fish ~/.config/fish/conf.d/
+cp -p conf.d/peco_select_repository_key_bindings.fish ~/.config/fish/conf.d/
 
 # alias
 echo 'alias convert_to_gif="convert_to_gif"' >> ~/.config/fish/config.fish
